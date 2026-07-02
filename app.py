4e79c38eb864ca58acf1fd843b9dc72fd8282a0e
mickeyd-358 <michdeft01@gmail.com>
2026-07-02 11:10:15 +1000

    Implement base api route to convert the integer to roman numeral

--- app.py
+++ app.py
@@ -10,5 +10,30 @@ app.config['SESSION_COOKIE_HTTPONLY'] = True  # Prevent XSS
 def home():
     return render_template('roman_converter.html')
 
+@app.route('/api/convert_integer', methods=['POST'])
+def convert_integer():
+    roman_dict = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1
+    }
+    final_str = ''
+
+    for roman, decimal in roman_dict.items():
+        final_str += (number // decimal) * roman
+        number -= decimal * (number // decimal)
+
+    return final_str
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
